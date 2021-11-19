ac7964a69def4b3b8a8f382553c75f4619ae8c12
xaviRodri <32159908+xaviRodri@users.noreply.github.com>
2021-11-19 22:39:34 +0100

    Implemented user cart workers
    We have added the ability to create new user carts and get them.
    
    When we call the `ExCashier.start_user_cart/1` function, we are creating a new user cart. This, as it's a supervised process, will have an internal state where we will store the user's id and all the items added to its cart.
    
    We also have two more functions:
    
    - `ExCashier.add_item/2` - which allows us to add an item to the user's cart
    - `ExCashier.get_cart/1` - that will return a user's cart
    
    Notes:
    
    - Added restriction for the user and item identifiers to be **strings**
    - All the user cart processes are registered under the user identifier in the `ExCashier.UserCartRegistry`
    - The previous restricts the number of carts per user to 1
    - Added tests for the mentioned features
    - Added some simple documentation and specs

---
+++ lib/ex_cashier/user_cart.ex
@@ -0,0 +1,39 @@
+defmodule ExCashier.UserCart do
+  @moduledoc """
+  This module defines a user cart process.
+
+  It also contains the needed handlers to operate with it.
+  """
+  use GenServer
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, nil, opts)
+  end
+
+  @impl true
+  def init(_) do
+    {:ok, %{user_id: nil, items: %{}}}
+  end
+
+  @impl true
+  def handle_call(:get_cart, _from, state) do
+    {:reply, state.items, state}
+  end
+
+  @impl true
+  def handle_cast({:add_item, item_id}, state) do
+    {:noreply, %{state | items: add_item(state.items, item_id)}}
+  end
+
+  # Adds an new item to the current item list (with quantity 1)
+  # or updates the current quantity of it (sums 1)
+  defp add_item(items_map, new_item) do
+    case Map.get(items_map, new_item) do
+      nil ->
+        Map.put(items_map, new_item, %{quantity: 1})
+
+      %{quantity: qty} = item ->
+        Map.put(items_map, new_item, %{item | quantity: qty + 1})
+    end
+  end
+end
