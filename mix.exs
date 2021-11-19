0a77b344a7bae18c7b3d3aa78a8f8a02a358fbc7
xaviRodri <32159908+xaviRodri@users.noreply.github.com>
2021-11-19 23:12:46 +0100

    Added code styling and test coverage checks
    As we want our application to be clean and as much tested as possible, we are adding the following libraries:
    
    - ExCoveralls - checks the test coverage
    - Credo - checks code styling

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule ExCashier.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -24,6 +31,8 @@ defmodule ExCashier.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excoveralls, "~> 0.14"},
+      {:credo, " ~> 1.6"}
     ]
   end
 end
